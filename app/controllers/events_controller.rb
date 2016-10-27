e964586fd252c7515a1d9558ef85da536e9518b7
nvpmai95 <nvpmai95@gmail.com>
2016-10-27 14:58:50 +0700

    Search by name in homepage

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,9 @@
 class EventsController < ApplicationController
   def index
     @events = Event.where('starts_at >= ?', DateTime.current)
+    if params[:search]
+      @events = Event.search(params[:search])
+    end
   end
 
   def show
