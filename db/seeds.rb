2ad189d51b4b16ca0ab14530de65acbc811e3d81
nvpmai95 <nvpmai95@gmail.com>
2016-10-27 12:34:45 +0700

    make event in seed start in the future

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,8 +22,8 @@ dalat = Venue.create({
 
 e = Event.create({
   name: 'Việt Nam Thử Thách Chiến Thắng', 
-  starts_at: DateTime.parse('Fri, 11 Mar 2016 7:00 AM+0700'),
-  ends_at: DateTime.parse('Sun, 13 Mar 2016 3:00 PM+0700'),
+  starts_at: DateTime.parse('Fri, 11 Nov 2016 7:00 AM+0700'),
+  ends_at: DateTime.parse('Sun, 13 Nov 2016 3:00 PM+0700'),
   venue: dalat,
   category: Category.find_by(name: 'Everything Else'),
   hero_image_url: 'https://az810747.vo.msecnd.net/eventcover/2015/10/25/C6A1A5.jpg?w=1040&maxheight=400&mode=crop&anchor=topcenter',
@@ -52,8 +52,8 @@ e = Event.create({
   name: 'Cảm ơn Đời - Live Concert Đan Trường', 
   venue: dan_venue,
   category: Category.find_by(name: 'Entertainment'),
-  starts_at: DateTime.parse('Sat, 16 Jan 2016, 8:00 PM+0700'),
-  ends_at: DateTime.parse('Sat, 16 Jan 2016, 10:30 PM+0700'),  
+  starts_at: DateTime.parse('Sat, 16 Dec 2016, 8:00 PM+0700'),
+  ends_at: DateTime.parse('Sat, 16 Dec 2016, 10:30 PM+0700'),  
   hero_image_url: 'https://az810747.vo.msecnd.net/eventcover/2015/12/11/C68636.jpg?w=1040&maxheight=400&mode=crop&anchor=topcenter',
   extended_html_description: <<-DESC
   <p style="text-align:justify"> </p>
